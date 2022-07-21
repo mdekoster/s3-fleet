fb4bc09051a38182034bfc6a334d743b36890bb5
Marcel de Koster <marcel.de.koster@logius.nl>
2022-07-21 16:44:47 +0200

    add comment

--- scripts/install-fleet.sh
+++ scripts/install-fleet.sh
@@ -1,4 +1,4 @@
-VERSION=0.3.9
+VERSION=0.3.10-rc4 # use 0.3.10-rc4 or above on 1.24 clusters
 helm -n fleet-system install --create-namespace --wait \
     fleet-crd https://github.com/rancher/fleet/releases/download/v${VERSION}/fleet-crd-${VERSION}.tgz
 helm -n fleet-system install --create-namespace --wait \
